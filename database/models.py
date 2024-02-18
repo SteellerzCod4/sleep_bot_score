afdbb1ccca484a8601e89512173df9ce10244e00
SteellerzCod4 <ilyastyllerz1996@yandex.ru>
2024-02-18 17:51:03 +0300

    change models, deleted all prev info and add new

--- database/models.py
+++ database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Date, Text, Boolean
+from sqlalchemy import Column, Integer, String, ForeignKey, Date, Text, Boolean, Float
 from sqlalchemy.orm import relationship
 
 from . import Base
@@ -9,13 +9,7 @@ class User(Base):
 
     id = Column(String(9), nullable=False, unique=True, primary_key=True)
     state = Column(String(20), default='START')
-    name = Column(String(30))
-    last_name = Column(String(30))
-    gender = Column(String(1))
-    link_photo = Column(String(50))
-    description = Column(Text(200))
-    location = Column(String(50))
-    is_active = Column(Boolean)
+    avg_score = Column(Float)
 
     def __repr__(self):
         return f"<User({self.id} {self.name})>"
