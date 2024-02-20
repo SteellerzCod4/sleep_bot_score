5bd8f4fa7e5cfb9c8b199ba6447fcfab6a542e09
SteellerzCod4 <ilyastyllerz1996@yandex.ru>
2024-02-20 21:27:59 +0300

    add enum for states

--- database/models.py
+++ database/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy.sql import func
-from sqlalchemy import Column, Integer, String, ForeignKey, Date, Text, Boolean, Float, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, Date, Text, Boolean, Float, DateTime, Enum
 from sqlalchemy.orm import relationship
+from bot_configs.states import States
 
 from . import Base
 
@@ -9,7 +10,7 @@ class User(Base):
     __tablename__ = 'user'
 
     id = Column(Integer, nullable=False, unique=True, primary_key=True)
-    state = Column(String(20), default='START')
+    state = Column(Enum(States), default=States.START.value)
     avg_score = Column(Float)
 
     time_info = relationship("TimeInfo", back_populates="user", foreign_keys="TimeInfo.user_id", cascade="all, delete")
@@ -21,11 +22,11 @@ class User(Base):
 class TimeInfo(Base):
     __tablename__ = 'time_info'
 
-    id = Column(Integer, nullable=False, unique=True, primary_key=True)
+    id = Column(Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
     user_id = Column(Integer, ForeignKey("user.id", ondelete="CASCADE"), nullable=False)
     retire_time = Column(DateTime(timezone=True), default=func.now())
     wakeup_time = Column(DateTime(timezone=True), default=func.now())
-    sleep_duration = Column(Integer)
+    sleep_duration = Column(Float)
     sleep_score = Column(Float)
 
     user = relationship("User", back_populates="time_info", uselist=False)
