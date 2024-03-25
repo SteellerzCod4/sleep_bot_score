8d337e08c6e8b7d772386f241a813005ff24d2e9
SteellerzCod4 <ilyastyllerz1996@yandex.ru>
2024-03-25 21:39:48 +0300

    Сократил код в operations добавив функцию setter_template

--- bot_configs/score_functions.py
+++ bot_configs/score_functions.py
@@ -3,7 +3,7 @@ from database.models import TimeInfo, TimeSettings
 import numpy as np
 
 
-def r2_retire_score(current_time: str, edge_time: str = "03:00", optimal_time: str = "22:30", ) -> float:
+def r2_retire_score(current_time: str, edge_time: str = "03:00", optimal_time: str = "22:30") -> float:
     current_time, edge_time = datetime.strptime(current_time, '%H:%M'), datetime.strptime(edge_time, '%H:%M')
     optimal_time = datetime.strptime(optimal_time, '%H:%M')
     if current_time >= optimal_time:
@@ -28,19 +28,7 @@ def sleep_duration_score(retire_time: str, wakeup_time: str, optimal_duration: f
     return 1 / (np.exp(np.square(sleep_duration.total_seconds() / 60 / 60 - optimal_duration) / 4))
 
 
-# def f1_sleep_score(retire_time: str, edge_retire_time: str, optimal_retire_time: str,
-#                    wakeup_time: str, edge_wakeup_time: str, optimal_wakeup_time: str,
-#                    optimal_duration: int, first_n: int) -> float:
-#     retire_score, wakeup_score, duration_score = (r2_retire_score(retire_time, edge_retire_time, optimal_retire_time),
-#                                                   r2_wakeup_score(wakeup_time, edge_wakeup_time, optimal_wakeup_time),
-#                                                   sleep_duration_score(retire_time, wakeup_time, optimal_duration))
-#     print(f"retire_score: {retire_score}\nwakeup_score: {wakeup_score}\nduration_score: {duration_score}")
-#     retire_score_fixed, wakeup_score_fixed = np.maximum(0, retire_score), np.maximum(0, wakeup_score)
-#     return np.round((3 * retire_score_fixed * wakeup_score_fixed * duration_score /
-#                      (retire_score_fixed + wakeup_score_fixed + duration_score)), first_n)
-
 def f1_sleep_score(current_wakeup_time: str, time_info: TimeInfo, time_settings: TimeSettings, first_n: int) -> float:
-
     retire_score, wakeup_score, duration_score = (r2_retire_score(time_info.current_retire_time,
                                                                   time_settings.worst_retire_time,
                                                                   time_settings.best_retire_time),
--- database/operations.py
+++ database/operations.py
@@ -7,6 +7,7 @@ from bot_configs.states import States
 db = get_db()
 
 
+# ============================= Getters =============================
 def get_user_by_id(user_id: int):
     """ Получает пользователя по его id """
     return db.query(User).filter(User.id == user_id).first()
@@ -20,6 +21,7 @@ def get_timeinfo_by_id(timeinfo_id: int):
 def get_timesettings_by_user_id(user_id: int):
     """ Получает TimeSettings юзера по его id """
     return db.query(TimeSettings).filter(TimeSettings.user_id == user_id).first()
+
 
 def get_timesettings_by_id(user_id):
     user = get_user_by_id(user_id)
@@ -42,46 +44,6 @@ def get_user_state(user_id: int):
     return States(user.state)
 
 
-def set_user_state(user_id: int, value: States):
-    """ Устанавливает состояние пользователя """
-    user = get_user_by_id(user_id)
-    if not user:
-        user = User(id=user_id, state=value)
-        db.add(user)
-
-    user.state = value
-    db.commit()
-
-    return user
-
-
-def set_user_name(user_id: int, user_name: str):
-    """ Устанавливает имя пользователя """
-    user = get_user_by_id(user_id)
-    if not user:
-        user = User(id=user_id, name=user_name)
-        db.add(user)
-
-    user.name = user_name
-    print(f"user.name: {user.name}")
-    db.commit()
-
-    return user
-
-
-def set_user_age(user_id: int, user_age: int):
-    """ Устанавливает возраст пользователя """
-    user = get_user_by_id(user_id)
-    if not user:
-        user = User(id=user_id, age=user_age)
-        db.add(user)
-
-    user.age = user_age
-    db.commit()
-
-    return user
-
-
 def get_timeinfo_by_user_id(user_id: int):
     user = get_user_by_id(user_id)
     print(f"user: {user}")
@@ -92,6 +54,59 @@ def get_timeinfo_by_user_id(user_id: int):
         return time_info
 
 
+# ============================= Setters =============================
+def setter_template(some_id, getter_func, attr_name, attr_value):
+    entity = getter_func(some_id)
+    setattr(entity, attr_name, attr_value)
+    db.commit()
+
+
+def set_user_state(user_id: int, value: States):
+    setter_template(user_id, get_user_by_id, "state", value)
+
+
+def set_user_name(user_id: int, user_name: str):
+    setter_template(user_id, get_user_by_id, "name", user_name)
+
+
+def set_user_age(user_id: int, user_age: int):
+    setter_template(user_id, get_user_by_id, "age", user_age)
+
+
+def set_user_current_retire_time(time_info_id: int, current_retire_time):
+    setter_template(time_info_id, get_timeinfo_by_id, "current_retire_time", current_retire_time)
+
+
+def set_user_best_retire_time(user_id: int, best_retire_time):
+    setter_template(user_id, get_timesettings_by_id, "best_retire_time", best_retire_time)
+
+
+def set_user_worst_retire_time(user_id: int, worst_retire_time):
+    setter_template(user_id, get_timesettings_by_user_id, "worst_retire_time", worst_retire_time)
+
+
+def set_user_best_wakeup_time(user_id: int, best_wakeup_time):
+    setter_template(user_id, get_timesettings_by_user_id, "best_wakeup_time", best_wakeup_time)
+
+
+def set_user_current_wakeup_time(user_id: int, current_wakeup_time):
+    setter_template(user_id, get_timeinfo_by_user_id, "current_wakeup_time", current_wakeup_time)
+
+
+def set_user_worst_wakeup_time(user_id: int, worst_wakeup_time):
+    setter_template(user_id, get_timesettings_by_user_id, "worst_wakeup_time", worst_wakeup_time)
+
+
+def set_user_best_duration_time(user_id: int, best_duration_time):
+    setter_template(user_id, get_timesettings_by_user_id, "best_sleep_duration", best_duration_time)
+
+
+def set_user_sleep_score(timeinfo: TimeInfo, sleep_score):
+    timeinfo.sleep_score = sleep_score
+    db.commit()
+
+
+# ============================= Creators =============================
 def create_new_time_info(user_id: int):
     user = get_user_by_id(user_id)
     new_time_info = TimeInfo(user_id=user_id,
@@ -101,57 +116,6 @@ def create_new_time_info(user_id: int):
     return new_time_info
 
 
-def set_user_current_retire_time(time_info_id: int, current_retire_time):
-    time_info = get_timeinfo_by_id(time_info_id)
-    print(time_info)
-    time_info.current_retire_time = current_retire_time
-    print(f"time_info.current_retire_time: {time_info.current_retire_time}")
-    db.commit()
-
-
-def set_user_best_retire_time(user_id: int, best_retire_time):
-    time_settings = get_timesettings_by_id(user_id)
-    print(f"time_settings: {time_settings}")
-    time_settings.best_retire_time = best_retire_time
-    db.commit()
-
-
-def set_user_worst_retire_time(user_id: int, worst_retire_time):
-    time_settings = get_timesettings_by_user_id(user_id)
-    time_settings.worst_retire_time = worst_retire_time
-    db.commit()
-
-
-def set_user_best_wakeup_time(user_id: int, best_wakeup_time):
-    time_settings = get_timesettings_by_user_id(user_id)
-    time_settings.best_wakeup_time = best_wakeup_time
-    db.commit()
-
-
-def set_user_current_wakeup_time(user_id: int, current_wakeup_time):
-    time_info = get_timeinfo_by_user_id(user_id)
-    time_info.current_wakeup_time = current_wakeup_time
-    db.add(time_info)
-    db.commit()
-
-
-def set_user_worst_wakeup_time(user_id: int, worst_wakeup_time):
-    time_settings = get_timesettings_by_user_id(user_id)
-    time_settings.worst_wakeup_time = worst_wakeup_time
-    db.commit()
-
-
-def set_user_best_duration_time(user_id: int, best_duration_time):
-    time_settings = get_timesettings_by_user_id(user_id)
-    time_settings.best_sleep_duration = best_duration_time
-    db.commit()
-
-
-def set_user_sleep_score(timeinfo: TimeInfo, sleep_score):
-    timeinfo.sleep_score = sleep_score
-    db.commit()
-
-
 def create_new_user(user_id: int):
     user = get_user_by_id(user_id)
     if not user:
@@ -159,9 +123,6 @@ def create_new_user(user_id: int):
         settings_time = TimeSettings(user_id=user_id)
         db.add(settings_time)
         db.commit()
-        print(f"settings_time: {settings_time}")
         user.current_settings_id = settings_time.id
-        print(f"user.current_settings_id: {user.current_settings_id}")
-        print(f"user.time_settings: {user.time_settings}")
         db.add(user)
         db.commit()
