3d96e467dae2170470e16e63d9229f5ed4c6a54e
SteellerzCod4 <ilyastyllerz1996@yandex.ru>
2024-03-25 19:27:14 +0300

    Переписал функцию f1_sleep_score, чтобы она принимала классы TimeInfo и TimeSettings

--- bot_configs/handlers/callback_handlers.py
+++ bot_configs/handlers/callback_handlers.py
@@ -18,24 +18,16 @@ async def call_back_data(callback: types.CallbackQuery):
         time_info_id = params[0]
         if button_text == msg.END_SLEEP_CALLBACK:
             current_wakeup_time = datetime.datetime.now().strftime("%H:%M")
+
             time_info = operations.get_timeinfo_by_id(time_info_id)
             time_settings = operations.get_timesettings_by_user_id(user_id)
-            print(f"time_info.current_retire_time in data: {time_info.current_retire_time}")
-            current_retire_time = time_info.current_retire_time
-            best_retire_time = time_settings.best_retire_time
-            worst_retire_time = time_settings.worst_retire_time
 
-            best_wakeup_time = time_settings.best_wakeup_time
-            worst_wakeup_time = time_settings.worst_wakeup_time
-
-            best_sleep_duration = time_settings.best_sleep_duration
-            # TODO: переписать функцию, чтобы она принимала только объект TimeInfo
-            sleep_score = f1_sleep_score(current_retire_time, worst_retire_time, best_retire_time,
-                                         current_wakeup_time, worst_wakeup_time, best_wakeup_time,
-                                         best_sleep_duration, 3)
+            sleep_score = f1_sleep_score(current_wakeup_time, time_info, time_settings, 3)
             print(f"sleep_score: {sleep_score}")
+
             operations.set_user_sleep_score(time_info, sleep_score)
             operations.set_user_state(user_id, States.START)
+
             await callback.answer(text=msg.SLEEP_SCORE_ACHIEVED_MES + str(sleep_score))
 
         elif button_text == msg.CANCEL_SLEEP_CALLBACK:
--- bot_configs/score_functions.py
+++ bot_configs/score_functions.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from database.models import TimeInfo, TimeSettings
 import numpy as np
 
 
@@ -20,20 +21,39 @@ def r2_wakeup_score(current_time: str, edge_time: str = "12:00", optimal_time: s
                 / (datetime.strptime(edge_time, '%H:%M') - datetime.strptime(optimal_time, '%H:%M')))
 
 
-def sleep_duration_score(retire_time: str, wakeup_time: str, optimal_duration: int) -> float:
+def sleep_duration_score(retire_time: str, wakeup_time: str, optimal_duration: float) -> float:
     wakeup_time, retire_time = datetime.strptime(wakeup_time, '%H:%M'), datetime.strptime(retire_time, '%H:%M')
     wakeup_time += timedelta(days=int(retire_time > wakeup_time))
     sleep_duration = wakeup_time - retire_time
     return 1 / (np.exp(np.square(sleep_duration.total_seconds() / 60 / 60 - optimal_duration) / 4))
 
 
-def f1_sleep_score(retire_time: str, edge_retire_time: str, optimal_retire_time: str,
-                   wakeup_time: str, edge_wakeup_time: str, optimal_wakeup_time: str,
-                   optimal_duration: int, first_n: int) -> float:
-    retire_score, wakeup_score, duration_score = (r2_retire_score(retire_time, edge_retire_time, optimal_retire_time),
-                                                  r2_wakeup_score(wakeup_time, edge_wakeup_time, optimal_wakeup_time),
-                                                  sleep_duration_score(retire_time, wakeup_time, optimal_duration))
+# def f1_sleep_score(retire_time: str, edge_retire_time: str, optimal_retire_time: str,
+#                    wakeup_time: str, edge_wakeup_time: str, optimal_wakeup_time: str,
+#                    optimal_duration: int, first_n: int) -> float:
+#     retire_score, wakeup_score, duration_score = (r2_retire_score(retire_time, edge_retire_time, optimal_retire_time),
+#                                                   r2_wakeup_score(wakeup_time, edge_wakeup_time, optimal_wakeup_time),
+#                                                   sleep_duration_score(retire_time, wakeup_time, optimal_duration))
+#     print(f"retire_score: {retire_score}\nwakeup_score: {wakeup_score}\nduration_score: {duration_score}")
+#     retire_score_fixed, wakeup_score_fixed = np.maximum(0, retire_score), np.maximum(0, wakeup_score)
+#     return np.round((3 * retire_score_fixed * wakeup_score_fixed * duration_score /
+#                      (retire_score_fixed + wakeup_score_fixed + duration_score)), first_n)
+
+def f1_sleep_score(current_wakeup_time: str, time_info: TimeInfo, time_settings: TimeSettings, first_n: int) -> float:
+
+    retire_score, wakeup_score, duration_score = (r2_retire_score(time_info.current_retire_time,
+                                                                  time_settings.worst_retire_time,
+                                                                  time_settings.best_retire_time),
+                                                  r2_wakeup_score(current_wakeup_time,
+                                                                  time_settings.worst_wakeup_time,
+                                                                  time_settings.best_wakeup_time),
+                                                  sleep_duration_score(time_info.current_retire_time,
+                                                                       current_wakeup_time,
+                                                                       time_settings.best_sleep_duration))
+
     print(f"retire_score: {retire_score}\nwakeup_score: {wakeup_score}\nduration_score: {duration_score}")
+
     retire_score_fixed, wakeup_score_fixed = np.maximum(0, retire_score), np.maximum(0, wakeup_score)
+
     return np.round((3 * retire_score_fixed * wakeup_score_fixed * duration_score /
                      (retire_score_fixed + wakeup_score_fixed + duration_score)), first_n)
